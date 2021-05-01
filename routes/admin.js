5f7e09fce5fad2be4e2682d01f07e364f1931e17
Sivin Saji <sivinsaji22@gmail.com>
2021-05-01 11:40:38 +0530

    In admin page we create a add-product button when we click this button we will go anothe page admin/add-product.hbs in this page we can see a form where we can  add new product here we use a choose imge option we set the action of form as '/admin/add-product/' to transfer a file we set enctype='multipart/format-data' in form. Then we install fileUpload module in project. we set router.post for form action and we print the data we entered in form and image details in console

--- routes/admin.js
+++ routes/admin.js
@@ -31,5 +31,11 @@ router.get('/', function(req, res, next) {
   ]
   res.render('admin/view-products',{admin:true,products});
 });
-
+router.get('/add-product',function(req,res){
+  res.render('admin/add-product')
+})
+router.post('/add-product',(req,res)=>{
+  console.log(req.body)
+  console.log(req.files.Image)
+})
 module.exports = router;
