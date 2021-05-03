651586f784b54a266dbcc7a855165cdd101ac73d
Sivin Saji <sivinsaji22@gmail.com>
2021-05-03 19:43:18 +0530

    Now we take the data from the DataBase and we display the data to user,admin. User will display the data in file 'user/view-products.hbs' and admin will display the product in 'admin/view-products.hbs

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,4 +1,5 @@
 var db=require('../config/connection')
+var collection=require('../config/collections')
 module.exports={
 
     addProduct:(product,callback)=>{
@@ -9,5 +10,11 @@ module.exports={
       callback(data.ops[0]._id)
       
     })
+    },
+    getAllProducts:()=>{
+      return new Promise(async(resolve,rejct)=>{
+        let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+        resolve(products)
+      })
     }
 }
