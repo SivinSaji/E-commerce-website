6d82260251b50f7b65c03a844a83a8261c88b2b4
Sivin Saji <sivinsaji22@gmail.com>
2021-05-03 18:14:14 +0530

    Image destination added now when we add an image to add product it will be saved on 'public/product-images'

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -2,10 +2,12 @@ var db=require('../config/connection')
 module.exports={
 
     addProduct:(product,callback)=>{
-    console.log(product)
+
 
     db.get().collection('product').insertOne(product).then((data)=>{
-      callback(true)
+      
+      callback(data.ops[0]._id)
+      
     })
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -36,10 +36,18 @@ router.get('/add-product',function(req,res){
   res.render('admin/add-product')
 })
 router.post('/add-product',(req,res)=>{
-  console.log(req.body)
-  console.log(req.files.Image)
-  productHelpers.addProduct(req.body,(result)=>{
-  res.write('product added')
+   productHelpers.addProduct(req.body,(id)=>{
+  
+    let image=req.files.Image
+    console.log(id)
+    image.mv('./public/product-images/'+id+'.jpg',(err,done)=>{
+      if(!err){
+        res.render('admin/add-product')
+      }else{
+        console.log(err);
+      }
+    })
+  res.render('admin/add-product')
   })
 })
 module.exports = router;
