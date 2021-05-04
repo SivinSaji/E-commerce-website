13b8b1bbc847cd1dcfe1be1c4afb89495e2c6bf2
Sivin Saji <sivinsaji22@gmail.com>
2021-05-04 20:49:14 +0530

    We created a button in user page to login or signup.Created page for login and page for signup

--- routes/user.js
+++ routes/user.js
@@ -5,8 +5,16 @@ var productHelpers=require('../helpers/product-helpers')
 router.get('/', function(req, res, next) {
   
   productHelpers.getAllProducts().then((products)=>{
-    res.render('user/view-products',{admin:true,products});       
+    res.render('user/view-products',{products});       
   })
 });
+router.get('/login',(req,res)=>{
+  res.render('user/login')
+})
+router.get('/signup',(req,res)=>{
+  res.render('user/signup')  
+})
+router.post('/signup',(req,res)=>{
 
+})
 module.exports = router;
